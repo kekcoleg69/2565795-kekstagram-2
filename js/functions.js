591dbfa1e95f55d3a6e025e1d0f768063e0b8a16
kekcoleg69 <kekcoleg69@gmail.com>
2025-05-15 12:44:19 +0300

    module2-task1 Решение функций (#1)

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+function checkLengthString(string, length) {
+  return string.length === length;
+}
+
+
+function checkPolindrome(string) {
+  let palindrome = '';
+  for(let i = string.length - 1;i >= 0; i--){
+    palindrome += string[i];
+  }
+  return palindrome.replaceAll(' ', '').toUpperCase() === string.replaceAll(' ', '').toUpperCase();
+}
+
+function returnNumber(string){
+  string = String(string);
+  let newNum = '';
+  for(let i = 0; i < string.length;i++){
+    if(!isNaN(Number(string[i])) && string[i] !== ' '){
+      newNum += string[i];
+    }
+  }
+  return newNum === '' ? NaN : Number(newNum);
+}
