086696e4fadaeaf9d09e6b24eeef8075dfeb4e72
kekcoleg69 <kekcoleg69@gmail.com>
2025-07-17 18:00:38 +0300

    Master (#9)
    * Рендер миниатюр
    
    * 1
    
    * 1

--- js/functions.js
+++ js/functions.js
@@ -1,31 +1,31 @@
 //Задания 2 модуля.
 
-// function checkLengthString(string, length) {
-//   return string.length === length;
-// }
-// checkLengthString('string', 5);
+function checkLengthString(string, length) {
+  return string.length === length;
+}
+checkLengthString('string', 5);
 
-// function checkPolindrome(string) {
-//   let palindrome = '';
-//   for(let i = string.length - 1;i >= 0; i--){
-//     palindrome += string[i];
-//   }
-//   return palindrome.replaceAll(' ', '').toUpperCase() === string.replaceAll(' ', '').toUpperCase();
-// }
-// checkPolindrome('string');
-// function returnNumber(string){
-//   string = String(string);
-//   let newNum = '';
-//   for(let i = 0; i < string.length;i++){
-//     if(!isNaN(Number(string[i])) && string[i] !== ' '){
-//       newNum += string[i];
-//     }
-//   }
-//   return newNum === '' ? NaN : Number(newNum);
-// }
-// returnNumber('42');
+function checkPolindrome(string) {
+  let palindrome = '';
+  for(let i = string.length - 1;i >= 0; i--){
+    palindrome += string[i];
+  }
+  return palindrome.replaceAll(' ', '').toUpperCase() === string.replaceAll(' ', '').toUpperCase();
+}
+checkPolindrome('string');
+function returnNumber(string){
+  string = String(string);
+  let newNum = '';
+  for(let i = 0; i < string.length;i++){
+    if(!isNaN(Number(string[i])) && string[i] !== ' '){
+      newNum += string[i];
+    }
+  }
+  return newNum === '' ? NaN : Number(newNum);
+}
+returnNumber('42');
 
-//Задание модуля 5.
+//Задание модуля 5..
 
 function isMeetingInWorkHours(startWorkTime, endWorkTime,startMeetingTime, endMeetingTime){
   startWorkTime = startWorkTime.split(':');
