2b1bc34dfcc17d77fd14fbff852a2e75c8abd224
kekcoleg69 <kekcoleg69@gmail.com>
2025-07-17 14:40:58 +0300

    1

--- js/functions.js
+++ js/functions.js
@@ -42,3 +42,8 @@ isMeetingInWorkHours('8:0', '10:0', '8:0', 120); // true
 isMeetingInWorkHours('08:00', '14:30', '14:00', 90); // false
 isMeetingInWorkHours('14:00', '17:30', '08:0', 90); // false
 isMeetingInWorkHours('8:00', '17:30', '08:00', 900); // false
+
+
+addEventListener('click', (evt) =>{
+  evt.preventDefault();
+});
