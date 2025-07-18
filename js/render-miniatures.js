63c09ea90c3d4f026c18ef158303d7913a3a3286
kekcoleg69 <kekcoleg69@gmail.com>
2025-07-18 14:22:30 +0300

    1

--- js/render-miniatures.js
+++ js/render-miniatures.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const picturesContainer = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content;
 const picture = pictureTemplate.querySelector('.picture');
@@ -6,14 +5,7 @@ export function renderMiniatures(photos) {
   const fragment = document.createDocumentFragment();
 
   for (const [index, photo] of photos.entries()) {
-=======
-const pictures = document.querySelector('.pictures');
-const pictureTemplate = document.querySelector('#picture').content;
-const picture = pictureTemplate.querySelector('.picture');
-export function renderMiniatures(photos){
-  const fragment = document.createDocumentFragment();
-  for(const photo of photos){
->>>>>>> 9f87a0f (Рендер миниатюр)
+
     const newElement = picture.cloneNode(true);
     const newElemImage = newElement.querySelector('.picture__img');
     const newElemComments = newElement.querySelector('.picture__comments');
@@ -23,7 +15,6 @@ export function renderMiniatures(photos){
     newElemImage.alt = photo.description;
     newElemComments.textContent = photo.comments.length;
     newElemLikes.textContent = photo.likes;
-<<<<<<< HEAD
 
     newElement.dataset.index = index;
 
@@ -32,10 +23,3 @@ export function renderMiniatures(photos){
 
   picturesContainer.appendChild(fragment);
 }
-=======
-    fragment.appendChild(newElement);
-  }
-  pictures.appendChild(fragment);
-}
-
->>>>>>> 9f87a0f (Рендер миниатюр)
