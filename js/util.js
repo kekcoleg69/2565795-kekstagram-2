bcaeac15ae91f6b601f847d789f8b2a7385793ea
kekcoleg69 <kekcoleg69@gmail.com>
2025-07-27 16:07:29 +0300

    module12-task1

--- js/util.js
+++ js/util.js
@@ -6,3 +6,10 @@ export function getRandomArrayElem(array){
   const indexElem = Math.floor(Math.random() * array.length);
   return array[indexElem];
 }
+export function debounce(callback, timeoutDelay = 500) {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback(...rest), timeoutDelay);
+  };
+}
